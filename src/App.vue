5caf8394314719b21ef3d0646264f0ebc37cf131
pappalaity <pappalaity@gmail.com>
2023-03-07 23:36:58 +0000

    components static

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+
+    <restaurant-row />
+    <!-- <restaurant-row /> -->
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import RestaurantRow from "./components/RestaurantRow.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RestaurantRow
   }
 }
 </script>
 
 <style>
+@import url('./styles/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/RestaurantRow.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="restaurant--row">
+        <h2 class="title">
+            Nos restaurants
+        </h2>
+        <div class="wrapper-content">
+            <restaurant-card v-for="(card, index) in 3" :key="index" />
+        </div>
+    </div>
+</template>
+
+<script>
+import RestaurantCard from './RestaurantCard.vue'
+export default {
+    components: { RestaurantCard },
+    name: "RestaurantRow"
+}
+</script>
+
+<style lang="scss">
+.wrapper-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
